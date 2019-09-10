f2e118c9685e86631cd5e715f0bf5ee2f74b8ca0
github_username <azizkoura@hotmail.com>
2019-09-10 12:20:58 +0200

    showing and hidding the modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -22,7 +22,8 @@ class BurgerBuilder extends Component {
       meat: 0
     },
     totalPrice: 4,
-    purchasable: false
+    purchasable: false,
+    purchasing: false
   }
 
   updatedPurchaseState = ingredients => {
@@ -71,6 +72,11 @@ class BurgerBuilder extends Component {
     this.updatedPurchaseState(updatedIngredient)
   }
 
+  purchaseHandler = () => {
+    let isPurchase = this.state.purchasing
+    this.setState({ purchasing: !isPurchase })
+  }
+
   render() {
     const disabledInfo = {
       ...this.state.ingredients
@@ -82,7 +88,7 @@ class BurgerBuilder extends Component {
 
     return (
       <Aux>
-        <Modal>
+        <Modal show={this.state.purchasing}>
           <OrderSummary ingredients={this.state.ingredients}/>
         </Modal>
         <Burger ingredients={this.state.ingredients}/>
@@ -92,6 +98,7 @@ class BurgerBuilder extends Component {
           disabled={disabledInfo}
           purchasable={this.state.purchasable}
           price={this.state.totalPrice}
+          ordered={this.purchaseHandler}
         />
       </Aux>
     )

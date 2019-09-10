ca389cb4ceeb3d351762a62888241c8d42bdec7d
github_username <azizkoura@hotmail.com>
2019-09-10 19:22:23 +0200

    structuring files

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,36 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 
-import Aux from '../../../hoc/Aux'
+import Aux from '../../../hoc/Aux/Aux'
 import Button from '../../UI/Button/Button'
 
-const orderSummary = props => {
-  const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
+class OrderSummary extends PureComponent {
+  componentDidUpdate() {
+    console.log('[OrderSummary], didUpdate')
+  }
+
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
+      return (
+        <li key={igKey}>
+          <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+        </li>
+      )
+    })
+
     return (
-      <li key={igKey}>
-        <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-      </li>
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious Burger with the following ingredients: </p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><span style={{fontWeight: 'bold'}}>Total Price: {this.props.totalPrice.toFixed(2)} €</span></p>
+        <p>Continue to checkout?</p>
+        <Button btnType="Danger" clicked={this.props.cancelled}>CANCEL</Button>
+        <Button btnType="Success" clicked={this.props.continued}>CONTINUE</Button>
+      </Aux>
     )
-  })
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious Burger with the following ingredients: </p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><span style={{fontWeight: 'bold'}}>Total Price: {props.totalPrice.toFixed(2)} €</span></p>
-      <p>Continue to checkout?</p>
-      <Button btnType="Danger" clicked={props.cancelled}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.continued}>CONTINUE</Button>
-    </Aux>
-  )
+  }
 }
 
-export default orderSummary
+export default OrderSummary
---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import classes from './Layout.css'
+
+import Aux from '../Aux/Aux'
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar'
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer'
+
+class Layout extends Component {
+  state = {
+    showSideDrawer: false
+  }
+
+  sideDrawerOpenHandler = () => {
+    this.setState({ showSideDrawer: true})
+  }
+
+  sideDrawerCloseHandler = () => {
+    this.setState(prevState => {
+      return { showSideDrawer: !prevState.showSideDrawer }
+    })
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar showingSideDrawer={this.sideDrawerOpenHandler}/>
+        <SideDrawer 
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerCloseHandler} 
+        />
+        <main className={classes.Content}>
+          { this.props.children }
+        </main>
+      </Aux>
+    )
+  }
+}
+
+export default Layout

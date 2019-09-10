36005cd99b1d32f8b8c14c8c112230ab2fde70ed
github_username <azizkoura@hotmail.com>
2019-09-10 10:26:01 +0200

    removing ingredients safely

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -43,14 +43,42 @@ class BurgerBuilder extends Component {
     this.setState({ ingredients: updatedIngredient, totalPrice: newPrice })
   }
 
-  removeIgredientHandler = (type) => {}
+  removeIgredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type]
+    if(oldCount <= 0) {
+      return
+    }
+    const updatedCount = oldCount - 1
+    const updatedIngredient = {
+      ...this.state.ingredients
+    }
+    updatedIngredient[type] = updatedCount
+
+    // calculate the price...
+    const priceDeduction = INGREDIENT_PRICES[type]
+    const oldPrice = this.state.totalPrice
+    const newPrice = oldPrice - priceDeduction
+
+    this.setState({ ingredients: updatedIngredient, totalPrice: newPrice })
+  }
 
   render() {
+    const disabledInfo = {
+      ...this.state.ingredients
+    }
+    for(let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0
+    }
+    // {salad: true, meat: false ...}
+
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients}/>
         <BuildControls 
-          ingredientAdded={this.addIngredientHandler}/>
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIgredientHandler}
+          disabled={disabledInfo}
+        />
       </Aux>
     )
   }

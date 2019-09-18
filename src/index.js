5ca539e76008d64df5b8da24ba8499f9c63449ea
github_username <azizkoura@hotmail.com>
2019-09-18 17:05:54 +0200

    installing the redux devTool

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
 import { BrowserRouter } from 'react-router-dom'
 import './index.css';
@@ -9,7 +9,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import reducer from './store/reducer'
 
-const store = createStore(reducer)
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 const app = (
   <Provider store={store}>

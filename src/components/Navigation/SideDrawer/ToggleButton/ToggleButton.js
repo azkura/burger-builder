80bf4d66863512e430c46bb85e0363d22316327c
github_username <azizkoura@hotmail.com>
2019-09-24 14:22:11 +0200

    improving sideDrawer

--- src/components/Navigation/SideDrawer/ToggleButton/ToggleButton.js
+++ src/components/Navigation/SideDrawer/ToggleButton/ToggleButton.js
@@ -2,7 +2,7 @@ import React from 'react'
 import classes from './ToggleButton.css'
 
 const toggleButton = (props) => (
-  <div className={classes.ToggleButton} onClick={props.click}>
+  <div className={classes.ToggleButton} onClick={props.clicked}>
     <div></div>
     <div></div>
     <div></div>
